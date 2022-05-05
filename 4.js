ef3dc3696aaf46bd9e07dc432e045463095eb68a
sanjanavamja18 <103937731+sanjanavamja18@users.noreply.github.com>
2022-05-04 17:28:50 -0700

    HTML & CSS CRASH course
    HTML & CSS CRASH course

--- 4.js
+++ 4.js
@@ -28,18 +28,22 @@ const calcTempAmp = function (remp) {
 const amp = calcTempAmp(temp);
 console.log(amp);
 
+/*Debuging with console*/
 const measureKelvin = function () {
   const measurement = {
     type: "temp",
     unit: "celsius",
-    value: prompt("Degree celsius:"),
+    value: Number(prompt("Degree celsius:")),
   };
   console.log(measurement);
-  console.table(measurement);
-
+  console.log(measurement);
+  console.log(measurement.value);
+  //console.warn(measurement.value);
+  //console.error(measurement.value);
   const kelvin = measurement.value + 273;
   return kelvin;
 };
+
 console.log(measureKelvin());
 
 /*Coding challange #1*/
